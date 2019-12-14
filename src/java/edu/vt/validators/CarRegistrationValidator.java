1d020ce9119f058664a9539aa07b8873f3d4a29b
jusmk96 <jusmk96@vt.edu>
2019-12-13 19:27:52 -0500

    Fixed merge conflict.

--- src/java/edu/vt/validators/CarRegistrationValidator.java
+++ src/java/edu/vt/validators/CarRegistrationValidator.java
@@ -6,10 +6,15 @@ package edu.vt.validators;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+
 import java.io.InputStreamReader;
+
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+
 import java.net.URL;
+
+
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
